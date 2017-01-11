24e0e34dc3cc7030af10e44c3d72af26d6c7f225
tokko <andreg@kth.se>
2017-01-11 17:08:57 +0100

    realtime timer on duration laol :D:D:D:D

---
+++ app/src/main/java/com/tokko/cameandwentv2/LocationFragment.java
@@ -0,0 +1,11 @@
+package com.tokko.cameandwentv2;
+
+
+import android.app.ListFragment;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment
+public class LocationFragment extends ListFragment{
+
+}
